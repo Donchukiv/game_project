16a2652c8b92f7e567fa56ff7c86419f19dcf593
Donchukiv <aescuch11@gmail.com>
2020-12-19 20:01:32 +0300

    appear function was added

--- bird.py
+++ bird.py
@@ -11,31 +11,39 @@ YELLOW = (225, 225, 0)
 PINK = (230, 50, 230)
 ORANGE = (255, 150, 100)
 
+#objects
+WIN_WIDTH = 500
+WIN_HEIGHT = 400
+sc = pygame.display.set_mode((WIN_HEIGHT, WIN_WIDTH), pygame.RESIZABLE)
+
 
 pygame.init()
 
 class bird:
-	def __init__(self, x, y, r, vx=1, vy, orient = right):
+	def __init__(self, x, y, r=10, color=ORANGE, vx=1, vy=0, orient = 'right'):
 		self.x = x
 		self.y = y
 		self.vx = vx
 		self.vy = vy
-		self.r = 10
-		self.color = 
-		self.id = pygame.draw.circle(sc, ORANGE, (x, y), r, color = ORANGE, tag = 'bird')
+		self.r = r
+		self.color = color
+		#self.id = pygame.draw.circle(sc, color, (x, y), r)
 		self.live = 1
 		self.orient = orient
 
+	def appear(self):
+		pygame.draw.circle(sc, self.color, (self.x, self.y), self.r)
+
 	def move(self):
-		while self.live = 1:
+		while self.live == 1:
 			self.x = self.x + self.vx
 			self.y = self.y + self.vy
 			self.vy = self.vy - 1
-			if self.x >= WIN_WIDTH or self.x <= 0:
+			if self.x + self.r >= WIN_WIDTH or self.x - self.r <= 0:
 				self.vx = (-1)*self.vx
 
 
 	def death(self):
 		if hittest():
 			self.live = 0
-			print(YOU DIED)
+			print('YOU DIED')
