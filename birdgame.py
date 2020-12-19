3c565996d0d28081f9a4316dd4df6e07dd9ab89e
Donchukiv <aescuch11@gmail.com>
2020-12-19 17:16:36 +0300

    Add function Create_Spikes to create lists of spikes

--- birdgame.py
+++ birdgame.py
@@ -1,29 +1,20 @@
-#birdgame, Python3,
-# 18.11.19
+import pygame
+import sys
 
-import tkinter as tk
-import math as m
-import random as *
-
-root = tk.Tk()
-fr = tk.Frame(root)
-root.geometry('300x400')
-canv = tk.Canvas(root, bg='white')
-canv.pack(fill=tk.BOTH, expand=1)
+pygame.init()
 
 
-class bird:
-	def __init__(self):
-		self.live = 1
-		self.color = choice(['pink','blue','green'])
-		self.x = x
-		self.y = y
-		self.vx = 5
-		self.vy = 0
-		self.id = canv.create_oval(
-                self.x - self.r,
-                self.y - self.r,
-                self.x + self.r,
-                self.y + self.r,
-                fill=self.color, 
-                tag = 'a')
+
+def main():
+	pygame.display.set_mode((400, 500), pygame.RESIZABLE) #window
+
+	while 1:
+		for i in pygame.event.get():
+			if i.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+
+		pygame.time.delay(20) #50 FPS
+
+if __name__ == '__main__':
+	main()
