a3974c37c1d6a716652c8d2e653eb4aac9414cf8
Donchukiv <aescuch11@gmail.com>
2020-12-19 22:04:24 +0300

    class menu was initiallized

---
+++ menu.py
@@ -0,0 +1,35 @@
+import pygame
+
+#colors
+WHITE =(255,255,255)
+BLACK = (0,0,0) 
+GRAY = (125, 125, 125)
+LIGHT_BLUE = (64, 128, 255)
+GREEN = (0, 200, 64)
+YELLOW = (225, 225, 0)
+PINK = (230, 50, 230)
+ORANGE = (255, 150, 100)
+
+#objects
+WIN_WIDTH = 500
+WIN_HEIGHT = 400
+sc = pygame.display.set_mode((WIN_HEIGHT, WIN_WIDTH), pygame.RESIZABLE)
+
+
+pygame.init()
+
+class menu:
+	def __init__(self, x, y, h=5, w=20 click = False):
+		self.x = x
+		self.y = y
+		self.click = click
+
+
+	def create_button(self, x, y, h, w,):
+		pass
+
+	def click(self, event):
+		pass
+
+	def appear(self):
+		pygame.draw.rect(sc, LIGHT_BLUE, (self.x, self.y, 20+self.x, 10+self.y))
