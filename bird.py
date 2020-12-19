b135087b8bf164e924e5efae9d920a25bb6d67c5
Donchukiv <aescuch11@gmail.com>
2020-12-19 19:40:24 +0300

    Add Draw functions with random choice.

--- bird.py
+++ bird.py
@@ -11,36 +11,31 @@ YELLOW = (225, 225, 0)
 PINK = (230, 50, 230)
 ORANGE = (255, 150, 100)
 
-#objects
-WIN_WIDTH = 500
-WIN_HEIGHT = 400
-sc = pygame.display.set_mode((WIN_HEIGHT, WIN_WIDTH), pygame.RESIZABLE)
-
 
 pygame.init()
 
 class bird:
-	def __init__(self, x, y, r=10, color=ORANGE, vx=1, vy=0, orient = 'right'):
+	def __init__(self, x, y, r, vx=1, vy, orient = right):
 		self.x = x
 		self.y = y
 		self.vx = vx
 		self.vy = vy
-		self.r = r
-		self.color = color
-		self.id = pygame.draw.circle(sc, color, (x, y), r)
+		self.r = 10
+		self.color = 
+		self.id = pygame.draw.circle(sc, ORANGE, (x, y), r, color = ORANGE, tag = 'bird')
 		self.live = 1
 		self.orient = orient
 
 	def move(self):
-		while self.live == 1:
+		while self.live = 1:
 			self.x = self.x + self.vx
 			self.y = self.y + self.vy
 			self.vy = self.vy - 1
-			if self.x + self.r >= WIN_WIDTH or self.x - self.r <= 0:
+			if self.x >= WIN_WIDTH or self.x <= 0:
 				self.vx = (-1)*self.vx
 
 
 	def death(self):
 		if hittest():
 			self.live = 0
-			print('YOU DIED')
+			print(YOU DIED)
--- birdgame.py
+++ birdgame.py
@@ -1,46 +1,20 @@
-#modules
 import pygame
-import spike
-import bird
-import menu
+import sys
 
-#colors
-WHITE =(255,255,255)
-BLACK = (0,0,0) 
-GRAY = (125, 125, 125)
-LIGHT_BLUE = (64, 128, 255)
-GREEN = (0, 200, 64)
-YELLOW = (225, 225, 0)
-PINK = (230, 50, 230)
-ORANGE = (255, 150, 100)
-BLUE = (0, 70, 225)
-
-#parametrs
-FPS = 50
-WIN_WIDTH = 500
-WIN_HEIGHT = 400
-
-#objects
 pygame.init()
-sc = pygame.display.set_mode((WIN_HEIGHT, WIN_WIDTH), pygame.RESIZABLE)
-b = bird.bird(40, 80)
+
 
 
 def main():
-	sc.fill(WHITE)#window
+	pygame.display.set_mode((400, 500), pygame.RESIZABLE) #window
 
 	while 1:
-		sc.fill(WHITE)#window
-
 		for i in pygame.event.get():
-			if i.type == pygame.QUIT: exit()
+			if i.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
 
 		pygame.time.delay(20) #50 FPS
-
-		pygame.display.update()
-
-		b.move()
-
 
 if __name__ == '__main__':
 	main()
